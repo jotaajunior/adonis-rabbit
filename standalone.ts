4db8126565d79a70daf96f40e54df2aea9ed9524
Jota <jjunior@protonmail.ch>
2020-10-18 21:57:24 -0300

    Exports for standalone usage

---
+++ standalone.ts
@@ -0,0 +1,5 @@
+export * from './src/RabbitManager'
+export * from './src/ConnectionManager'
+export * from './src/Messsage'
+export * from './src/Exceptions/InvalidRabbitConfigException'
+export * from './src/Exceptions/NullMessageException'
